65cf6fa8ada6fc383fc2e280e811d6a4b83e4f8e
Rakhman Imanov <raxman.i@mail.ru>
2025-07-01 01:31:13 +0300

    Написана простейшая реализация основных кнопок бота для режима пользователя

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
 import telebot
 import json
+import texts, markups
+
 
 config_file = open('config.json', 'r')
 config = json.loads(config_file.read())
-
 config_file.close()
 
-bot = telebot.TeleBot(token=config['api_token'])
 
-@bot.message_handler()
-def handle_text_messages(message):
-    bot.send_message(chat_id=message.chat.id, text=message.text)
+bot = telebot.TeleBot(token=config['api_token'])
+users = []
+session_started = False
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    print(markups.register_markup)
+    bot.send_message(chat_id=message.chat.id, text=texts.start, reply_markup=markups.register_markup)
+
+
+@bot.callback_query_handler()
+def handle_callback_query(callback):
+    match(callback.data):
+        case markups.CallbackTypes.register.value:
+            if not session_started:
+                bot.edit_message_text(
+                    chat_id=callback.message.chat.id, 
+                    message_id=callback.message.id,
+                    text=texts.current_settings(len(users)),
+                    reply_markup=markups.active_session_markup
+                )
+
+            else:
+                bot.edit_message_text(
+                    chat_id=callback.message.chat.id, 
+                    message_id=callback.message.id,
+                    text=texts.sessiong_already_started
+                )
+
+        case markups.CallbackTypes.leave_session.value:
+            bot.edit_message_text(
+                chat_id=callback.message.chat.id, 
+                message_id=callback.message.id,
+                text=texts.user_left_session, 
+                reply_markup=markups.register_markup
+            )
+
 
 bot.infinity_polling()
---
+++ texts.py
@@ -0,0 +1,6 @@
+start = "Нажмите на кнопку для регистрации на нетворкинг сессию"
+sessiong_already_started = "Сессия уже началась, регистрация закрыта"
+user_left_session = "Вы покинули нетворкинг сессию"
+
+def current_settings(tables_count: int):
+    return f"Количество столов: {tables_count}"
